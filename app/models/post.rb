d4de1ec61fc639e11f27ab512c1300c8be834bb0
kamasuJL <156296235+kamasuJL@users.noreply.github.com>
2024-04-07 14:50:09 +0000

    [Add]post_view

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
+  
+  # validates :title, presence: true
+  # validates :phase, presence: true
+  # validates :body, presence: true
+  # validates :way, presence: true
+  # validates :investment, presence: true
 end
