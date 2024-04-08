0ce18b52870136bedfeb0637c3f0fed5557de8e6
kamasuJL <156296235+kamasuJL@users.noreply.github.com>
2024-04-08 01:39:22 +0000

    [Add]edit.view

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,4 +1,11 @@
 class Public::UsersController < ApplicationController
   def mypage
+    @posts = current_user.posts
+    @user = current_user
   end
+  
+  def edit
+    @user = current_user
+  end
+
 end
