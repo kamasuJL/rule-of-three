369114e3ae7f7febe669a1fef8ad27d867230e94
kamasuJL <156296235+kamasuJL@users.noreply.github.com>
2024-04-08 08:37:00 +0000

    [Add]destroy_button

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -15,11 +15,27 @@ class Public::PostsController < ApplicationController
   
   def show
     @post = Post.find(params[:id])
+    @user = @post.user
   end
   
   def index
     @posts = Post.all
   end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render "edit"
+    end
+  end
+  
+  
   
   private
 
