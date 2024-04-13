79ff299a8bd508855ba6970371487d9404ad3a24
kamasuJL <156296235+kamasuJL@users.noreply.github.com>
2024-04-13 01:57:34 +0000

    [Fix]comment非同期

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -2,10 +2,9 @@ class Public::CommentsController < ApplicationController
   
   def create
     post = Post.find(params[:post_id])
-    comment = current_user.comments.new(comment_params)
-    comment.post_id = post.id
-    comment.save
-    redirect_to post_path(post)
+    @comment = current_user.comments.new(comment_params)
+    @comment.post_id = post.id
+    @comment.save
   end
   
   def destroy
