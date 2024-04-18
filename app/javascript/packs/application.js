1dca95fab4af48d2afeb6724dda91446b9e24063
kamasuJL <156296235+kamasuJL@users.noreply.github.com>
2024-04-18 08:13:21 +0000

    [Fix]search

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ import "bootstrap";
 import "../stylesheets/application";
 import "../stylesheets/button"; 
 import "../stylesheets/about"; 
+import "../stylesheets/search"; 
 
 Rails.start()
 Turbolinks.start()
